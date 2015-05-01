e90f0d5e22ad702a22252bec2fd5181f0d8709aa
Calvin Cheng <linchuan.cheng@gmail.com>
2015-05-01 21:10:35 +0800

    Use NSLog to show where we are in the Bonjour publishing lifecycle

--- BonjourPublishServices/BonjourPublishServices.m
+++ BonjourPublishServices/BonjourPublishServices.m
@@ -13,11 +13,14 @@
 
 -(void)startServiceOfType:(NSString *)type andPort:(int)port {
     self.status = BONJOURPUBLISHSTOPPED;
+    // leave name as @"" so we will display the device name
     self.service = [[NSNetService alloc] initWithDomain:@"" type:type name:@"" port:port];
     if (self.service) {
+        NSLog(@"Begin to setDelegate and publish");
         [self.service setDelegate:self];
         [self.service publish];
     } else {
+        NSLog(@"Unable to publish");
         [[NSNotificationCenter defaultCenter] postNotificationName:BONJOURNOTIFICATIONSTOP object:nil];
     }
 }
@@ -31,6 +34,7 @@
 
 // this method is called prior to the service being published through Bonjour
 -(void)netServiceWillPublish:(NSNetService *)sender {
+    NSLog(@"Going to be published");
     self.status = BONJOURPUBLISHSTOPPED;
 }
 
@@ -38,17 +42,20 @@
 -(void)netServiceDidPublish:(NSNetService *)sender {
     self.status = BONJOURPUBLISHSTARTED;
     self.publishedName = sender.name;
+    NSLog(@"Published %@", self.publishedName);
     [[NSNotificationCenter defaultCenter] postNotificationName:BONJOURNOTIFICATIONSTART object:nil];
 }
 
 // ping the notification center if the net service fails to publish
 -(void)netService:(NSNetService *)sender didNotPublish:(NSDictionary *)errorDict {
+    NSLog(@"Failed to publish");
     self.status = BONJOURPUBLISHSTOPPED;
     [[NSNotificationCenter defaultCenter] postNotificationName:BONJOURNOTIFICATIONSTOP object:nil];
 }
 
 // ping the notification center if the net service stops
 -(void)netServiceDidStop:(NSNetService *)sender {
+    NSLog(@"Service stopped");
     self.status = BONJOURPUBLISHSTOPPED;
     [[NSNotificationCenter defaultCenter] postNotificationName:BONJOURNOTIFICATIONSTOP object:nil];
 }
